2e09bccad7f97d8fca1c53c7ba9abfeb052e59d4
FROM SOL프롬 솔 <leesol529@gmail.com>
2019-12-13 16:40:49 +0900

    scss 파일 추가

--- src/components/Base/Header.js
+++ src/components/Base/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.jpg';
-import './Header.css';
-import SearchField from 'react-search-field';
+import ReactSearchBox from 'react-search-box'
 
 export default class Header extends React.Component{
     render(){
         return(
             <div>
-                <logo className="logo"/>
-                <SearchField placeholder="어디로 떠나고 싶으신가요?"/>
+                <img src={logo} alt="logo" className="logo"/>
+                <ReactSearchBox placeholder="어디로 떠나고 싶으신가요?"
+                                className="search_bar"/>
             </div>
         );
     }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '../Base/Header';
 
 export default class Main extends React.Component{
     render(){
         return(
             <div>
-                main
+                <Header />
             </div>
         );
     }
