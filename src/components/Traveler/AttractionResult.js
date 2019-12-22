ce05a74ecc84754ceb028429e079eb40b2dfc995
FROM SOL프롬 솔 <leesol529@gmail.com>
2019-12-22 15:55:53 +0900

    /traveler/choice를 위한 result 컴포넌트 생성 및 이미지 추가

---
+++ src/components/Traveler/AttractionResult.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class AttractionResult extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            image: ""
+        }
+    }
+
+    componentWillMount(){
+        //Byte array 이미지를 이미지로 바꾸는 것.
+    let url = "http://localhost:9000/image/"+this.props.att.img;
+    axios
+        .get(
+            url,
+            { responseType: 'arraybuffer' },
+        )
+        .then(response => {
+            const base64 = btoa(
+                new Uint8Array(response.data).reduce(
+                    (data, byte) => data + String.fromCharCode(byte),
+                    '',
+                ),
+            );
+            this.setState({ image: "data:;base64," + base64 });
+        });
+    }
+
+    render(){
+        console.log(this.props.att)
+        return(
+            <table className="tChoice_table" id="choiceFrm">
+                <thead>
+                    <tr>
+                        <th>
+                            <input type="checkbox"/>
+                            Attraction{this.props.idx}
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="tChoice_input tChoice_center">
+                            <div className="thumbnail-wrapper">
+                                <div className="thumbnail">
+                                    <img src={this.state.image} 
+                                        className="tChoice_img" 
+                                        alt="result_img"/>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="tChoice_input tChoice_center">
+                            {this.props.att.name}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="tChoice_input">
+                            {this.props.att.content}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+}
---
+++ src/components/Traveler/RestaurantResult.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class RestaurantsResult extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            image: ""
+        }
+    }
+
+    componentWillMount(){
+        //Byte array 이미지를 이미지로 바꾸는 것.
+    let url = "http://localhost:9000/image/"+this.props.res.img;
+    axios
+        .get(
+            url,
+            { responseType: 'arraybuffer' },
+        )
+        .then(response => {
+            const base64 = btoa(
+                new Uint8Array(response.data).reduce(
+                    (data, byte) => data + String.fromCharCode(byte),
+                    '',
+                ),
+            );
+            this.setState({ image: "data:;base64," + base64 });
+        });
+    }
+
+    render(){
+        return(
+            <table className="tChoice_table" id="choiceFrm">
+                <thead>
+                    <tr>
+                        <th>
+                            <input type="checkbox"/>
+                            Restaurant{this.props.idx}
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="tChoice_input tChoice_center">
+                            <div className="thumbnail-wrapper">
+                                <div className="thumbnail">
+                                    <img src={this.state.image} 
+                                    className="tChoice_img" 
+                                    alt="result_img"/>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="tChoice_input">
+                            <b style={{margin:'-5px'}}>Type</b> : {this.props.res.type} activity
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="tChoice_input">
+                            <b style={{marginLeft:'-5px'}}>Price range</b>: {this.props.res.price}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="tChoice_input">
+                            {this.props.res.content}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+}
