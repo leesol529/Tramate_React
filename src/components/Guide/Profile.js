ddc390ec5c47476deed44a75fd13b62a060d417f
DESKTOP-J0M2VCI\ab586 <ab5867@naver.com>
2020-01-07 19:54:54 +0900

    Guide/Profile.js, Introduce.js, Profile.scss 와 Traveler/TravelerProdile.js 수정. 그리고 spot.png 사진 추가

--- src/components/Guide/Profile.js
+++ src/components/Guide/Profile.js
@@ -1,78 +1,79 @@
 import React from 'react';
 import axios from 'axios';
-import host1 from '../../img/host1.jpg';
 import check1 from '../../img/checked.svg'
 import uncheck1 from '../../img/unchecked.svg';
 import chat1 from '../../img/chat.png';
 import calendar from '../../img/calendar.png';
-import { Link } from 'react-router-dom';
-
-export default class Profile extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            //spring 에서 게시판 목록을 받아서 저장할 변수
-            guideData: '',
-            guideratedata: ''
-
-        };
-    }
+import {Link} from 'react-router-dom';
+import spot from '../../img/spot.png';
 
 
+export default class Profile extends React.Component{
+   
+    constructor(props){
+       super(props);
+       this.state={
+           //spring 에서 게시판 목록을 받아서 저장할 변수
+           guideData: '',
+           guideratedata:''
+           
+       };
+   }
 
-    //목록 가져올 메소드
-    Gdata = () => {
-        //spring에서 json파일 결과물 나오는 url
-        var url = "http://192.168.0.89:9000/guide/select";//spring 주소가 9000임. 9000에서 가져오라는 의미.
-        let data = new FormData();
-        data.append("num", this.props.gnum);
-        axios.post(url, data).then((responseData) => {
-            //spring 서버로부터 받은 데이터로 guestData 수정
-            this.setState({
-                guideData: responseData.data
+
+
+   //목록 가져올 메소드
+   Gdata=()=>{
+       //spring에서 json파일 결과물 나오는 url
+       var url="http://192.168.0.89:9000/guide/select";//spring 주소가 9000임. 9000에서 가져오라는 의미.
+       let data = new FormData();
+       data.append("num", this.props.gnum);
+       axios.post(url, data).then((responseData)=>{
+           //spring 서버로부터 받은 데이터로 guestData 수정
+           this.setState({
+               guideData: responseData.data
             })
             console.log(this.state.guideData);
-
-        }).catch((error) => {
+    
+        }).catch((error)=>{
             console.log("**guide data 오류**");
         });
-    }
+   }
 
-    /* GuideRate 별 댓글 갯수 가져오는 메소드 */
-    GRate = () => {
-        var url = "http://192.168.0.89:9000/guiderate/select";
-        let GRatedata = new FormData();
-        GRatedata.append("gnum", this.props.gnum);
-        axios.post(url, GRatedata).then((responseData) => {
-            this.setState({
-                guideratedata: responseData.data
-            })
-            console.log(this.state.guideratedata);
-        }).catch((error) => {
-            console.log("** guideratedata 오류");
-        })
-    }
+   /* GuideRate 별 댓글 갯수 가져오는 메소드 */
+   GRate=()=>{
+       var url="http://192.168.0.89:9000/guiderate/select";
+       let GRatedata = new FormData();
+       GRatedata.append("gnum",this.props.gnum);
+       axios.post(url, GRatedata).then((responseData)=>{
+           this.setState({
+               guideratedata:responseData.data
+           })
+           console.log(this.state.guideratedata);
+       }).catch((error)=>{
+           console.log("** guideratedata 오류");
+       })
+   }
 
-    componentWillMount() {
-        this.Gdata();
-        this.GRate();
-    }
-
-
-    render() {
-        const img = this.state.guideData.img;
-        const email = this.state.guideData.email;
-        const mobile = this.state.guideData.mobile;
-        const user = localStorage.getItem("user");
+   componentWillMount(){
+       this.Gdata();
+       this.GRate();
+   }
+   
+   
+    render(){
+        const img=this.state.guideData.img;
+        const email=this.state.guideData.email;
+        const mobile=this.state.guideData.mobile;
+        const user=localStorage.getItem("user");
         const goSchedule1 = <Link to={`/guide/schedule/${localStorage.getItem('gnum')}`} className="goSchedule">스케쥴 보러가기</Link>;
-        const noSchedule = null;
-        const guideSchedule = user ? goSchedule1 : noSchedule;
+        const noSchedule=null;
+        const guideSchedule=user?goSchedule1:noSchedule;
 
-        return (
+        return(
             <div className="boss-guideprofile">
                 <div className="main-photo">
-                    <img src={host1} className="guide-profile-image" alt="" />
+                    <img src={`http://192.168.0.89:9000/image/${img}`} className="guide-profile-image" alt=""/>
                 </div>
                 {/* 세로줄 */}
                 <div className="vertical-line1">
@@ -80,22 +81,30 @@ export default class Profile extends React.Component {
 
                 <div className="wrap1">
                     <div className="wrap1-count-review">
-                        <img src={chat1} className="chat1" alt="" />
+                        <img src={chat1} className="chat1" alt=""/>
                         <p className="review-count">
-                            <a href="#abc" className="ahref-Profile">후기 {this.state.guideratedata}개</a>
+                           <a href="#abc" className="ahref-Profile">후기 {this.state.guideratedata}개</a> 
                         </p>
                     </div>
 
-                    {/* 가이드 스케줄 확인할 수 있는 메뉴 */}
+                     {/* 가이드 스케줄 확인할 수 있는 메뉴 */}
                     <div className="wrap1-guideSchedule">
-                        <img src={calendar} className="calendar" alt="" />
+                        <img src={calendar} className="calendar" alt=""/>
                         {guideSchedule}
                     </div>
 
+                    {/* 가이드 장소 */}
+                    <div className="wrap1-guidePlace">
+                        <img src={spot} className="check-boss-place" alt=""/>
+                        <p className="guidePlace">
+                        <Link to={`/guide/choice/${localStorage.getItem('gnum')}`} className="goSchedule2">가이드 장소 추가</Link>
+                        </p>
+                    </div>
+
                     <div className="wrap1-checked-af">
-                        <img src={check1} className="check-boss" alt="" />
+                        <img src={check1} className="check-boss" alt=""/>
                         <p className="authentification">
-                            인증 완료
+                             인증 완료
                         </p>
                     </div>
                 </div>
@@ -105,9 +114,9 @@ export default class Profile extends React.Component {
                 <div className="af-info">
                     <h4 className="af-info2"> {this.state.guideData.name}님의 인증 내역</h4>
                     <ul>
-                        <li className="profile-flexbox">{img ? <img src={check1} className="check" alt="" /> : <img src={uncheck1} className="uncheck" alt="" />}&nbsp;<div className="sellca-profile">셀카</div></li>
-                        <li className="profile-flexbox">{email ? <img src={check1} className="check" alt="" /> : <img src={uncheck1} className="uncheck" alt="" />}&nbsp;<div className="email-profile">이메일</div></li>
-                        <li className="profile-flexbox">{mobile ? <img src={check1} className="check" alt="" /> : <img src={uncheck1} className="uncheck" alt="" />}&nbsp;<div className="addr-profile">전화번호</div></li>
+                        <li className="profile-flexbox">{img ? <img src={check1} className="check" alt=""/>:<img src={uncheck1} className="uncheck" alt=""/>}&nbsp;<div className="sellca-profile">셀카</div></li>
+                        <li className="profile-flexbox">{email ? <img src={check1} className="check" alt=""/>:<img src={uncheck1} className="uncheck" alt=""/>}&nbsp;<div className="email-profile">이메일</div></li>
+                        <li className="profile-flexbox">{mobile ? <img src={check1} className="check" alt=""/>:<img src={uncheck1} className="uncheck" alt=""/>}&nbsp;<div className="addr-profile">전화번호</div></li>
                     </ul>
                 </div>
             </div>
